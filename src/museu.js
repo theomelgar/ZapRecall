6e96dff7fcd7f52744235933b380560c9eaa24b6
theomelgar <theomelgar@gmail.com>
2022-12-06 16:36:06 -0300

    att de pergunta do museu

--- src/museu.js
+++ src/museu.js
@@ -5,7 +5,7 @@ const museu = [
 	{ question: "Por quem e quando ele foi fundado?", answer: "Dom João VI (1816-1826)" },
 	{ question: "Graças a um meteiorito, durante o Segundo Reinado, o museu entrou no seu auge. Qual o nome do meteorito?", answer: "Meteorito do Bendegó" },
 	{ question: "De qual periodo eram as rochas sedimentares antigas que foram descobertas na Antártida", answer: "Cretáceo" },
-	{ question: "Qual era o nome da múmia, que não ficava dentro do sarcófago, que fez parte do acervo do Egito Antigo?", answer: "Kherima" },
+	{ question: "Qual era o nome da única múmia, que fazia parte do acervo do Egito Anitgo, cuja a esquife nunca foi aberta?", answer: "Sha-Amun-en-su" },
 	{ question: "Tribo indígena brasileira que fabricavam máscaras", answer: "Ticunas" }
 ]
 export default museu
